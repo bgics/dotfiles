fae8bc1a987e5c8b99640467a4911953413213ed
Bhuvansh Goyal <bhuvansh.iitb@gmail.com>
2025-08-03 18:19:32 +0000

    added wallpaper and actual contents for wezterm config

--- wezterm/.config/wezterm/color_schemes.lua
+++ wezterm/.config/wezterm/color_schemes.lua
@@ -1,1 +1,35 @@
-/mnt/c/Users/bhuva/.config/wezterm/color_schemes.lua
+local M = {}
+
+M["Gruvbox Material Dark"] = {
+		foreground = "#D4BE98",
+		background = "#282828",
+		cursor_bg = "#D4BE98",
+		cursor_fg = "#282828",
+		cursor_border = "#D4BE98",
+		selection_fg = "#282828",
+		selection_bg = "#D4BE98",
+		scrollbar_thumb = "#665c54",
+		split = "#665c54",
+		ansi = {
+			"#3C3836",
+			"#EA6962",
+			"#A9B665",
+			"#E78A4E",
+			"#7DAEA3",
+			"#D3869B",
+			"#89B482",
+			"#D4BE98",
+		},
+		brights = {
+			"#665C54",
+			"#EA6962",
+			"#A9B665",
+			"#E3A84E",
+			"#7DAEA3",
+			"#D3869B",
+			"#89B482",
+			"#DFBF8E",
+		},
+}
+
+return M
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,1 +1,24 @@
-/mnt/c/Users/bhuva/.wezterm.lua
+local wezterm = require("wezterm")
+local color_schemes = require("color_schemes")
+
+local config = wezterm.config_builder()
+
+config.default_prog = { "wsl.exe", "--cd", "~", "--", "bash", "-l", "-c", "nu" }
+
+config.color_schemes = color_schemes
+config.color_scheme = 'Gruvbox Material Dark'
+
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.font = wezterm.font("CodeNewRoman Nerd Font")
+config.font_size = 14
+
+config.window_padding = {
+  left = 8,
+  top = 8,
+  right = 8,
+  bottom = 8,
+}
+
+return config
